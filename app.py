aeeb93e429bdc65a0d0f52067992a7e1a344f72e
nasircolon <nasircolon30@gmail.com>
2025-04-09 21:31:57 +0000

    code with flask, index page, empty css/styling for ui

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, jsonify, session
+from flask_session import Session
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+session(app)
+
+# Home route
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+# Clear session route
+@app.route('/clear_session', methods=['GET'])
+def clear_session():
+    # Clear the session
+    session.clear()
+    return jsonify({'status': 'session cleared'})
